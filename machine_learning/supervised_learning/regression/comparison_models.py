039d748c50ef5fb4a321933fbba28837bab1f5ae
Elior Ben-Yosef <eliorby@gmail.com>
2021-08-24 14:29:34 +0300

    UPDATE

--- machine_learning/supervised_learning/regression/comparison_models.py
+++ machine_learning/supervised_learning/regression/comparison_models.py
@@ -9,34 +9,21 @@ def univariate_models(dataset, is_linear=True, is_nonlinear=True):
     X_range = np.arange(min(dataset.X), max(dataset.X) + 0.1, 0.1)[:, np.newaxis]
 
     regression_models = RegressionModels(dataset.X_train, dataset.y_train, dataset.X_test, dataset.y_test, X_range)
+    if is_linear:
+        regression_models.all_linear()
+    if is_nonlinear:
+        regression_models.all_nonlinear()
 
     visualizer = Visualizer(dataset.X_test, dataset.y_test, dataset.x_labels[0], None, dataset.y_label)
-
     if is_linear:
-        regression_models.lin_reg()
-        regression_models.ridge_reg()
-        regression_models.lin_svr()
-
         visualizer.visualize_results_2D(regression_models.predictions)
-
     if is_nonlinear:
-        regression_models.poly_reg()
-        regression_models.poly_reg(pol_deg=3)
-        regression_models.poly_reg(pol_deg=4)
-        # regression_models.svr(kernel='poly', deg=3)
-        regression_models.kernel_svr()
-        regression_models.dtr()
-        regression_models.rfr()
-
         visualizer.visualize_results_2D(regression_models.predictions_fine, X_range)
 
 
 def bivariate_models(dataset):
     regression_models = RegressionModels(dataset.X_train, dataset.y_train, dataset.X_test, dataset.y_test)
-
-    regression_models.lin_reg()
-    regression_models.ridge_reg()
-    regression_models.lin_svr()
+    regression_models.all_linear()
 
     visualizer = Visualizer(dataset.X_test, dataset.y_test, *dataset.x_labels, dataset.y_label)
     visualizer.visualize_results_3D(regression_models.predictions, 1)
@@ -46,23 +33,8 @@ def bivariate_models(dataset):
 
 def multivariate_models(dataset):
     regression_models = RegressionModels(dataset.X_train, dataset.y_train, dataset.X_test, dataset.y_test)
-
-    regression_models.lin_reg()  # , print_formula=False
-    regression_models.ridge_reg()
-    regression_models.lin_svr()  # , print_formula=False
-
-    regression_models.poly_reg()
-    regression_models.poly_reg(pol_deg=3)
-    regression_models.poly_reg(pol_deg=4)
-    regression_models.kernel_svr(kernel='poly', pol_deg=2)
-    regression_models.kernel_svr(kernel='poly')
-    regression_models.kernel_svr(kernel='poly', pol_deg=4)
-    regression_models.kernel_svr()
-    regression_models.dtr()
-    regression_models.rfr()
-
+    regression_models.all()
     regression_models.print_models_r2()
-
     regression_models.print_results()
 
 
@@ -72,21 +44,16 @@ def example_univariate_models():
     dataset = RegressionDataSets()
 
     # Linear models
-
     dataset.get_Salary_Data()
     univariate_models(dataset, is_nonlinear=False)
-
     dataset.get_linreg_simple()
     univariate_models(dataset, is_nonlinear=False)
-
     dataset.get_diabetes(indices=[2])  # Use only one feature
     univariate_models(dataset, is_nonlinear=False)
 
     # Nonlinear models
-
     dataset.get_Position_Salaries()
     univariate_models(dataset, is_linear=False)
-
     dataset.get_Combined_Cycle_Power_Plant(indices=[1])  # use only one feature
     univariate_models(dataset, is_linear=False)
 
@@ -103,7 +70,6 @@ def example_multivariate_models():
 
     dataset.get_Combined_Cycle_Power_Plant()
     multivariate_models(dataset)
-
     dataset.get_Startups()
     multivariate_models(dataset)
 
